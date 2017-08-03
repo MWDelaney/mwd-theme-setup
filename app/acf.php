f6b865cf44187b2e0ddbdc49a8704adad890dee2
Michael W. Delaney <michael.delaney@gmail.com>
2017-08-03 11:16:52 -0400

    Update acf.php

--- app/acf.php
+++ app/acf.php
@@ -6,21 +6,33 @@ class ACF {
 
 	function __construct() {
 
+    // The theme's acf-json directory
+    $theme_dir = get_stylesheet_directory() . '/assets/acf-json';
+
+    // This plugin's skeleton acf-json directory
+    $plugin_dir = MWD_SETUP_PLUGIN_DIR . '/assets/acf-json';
+
 		// Hide ACF menu item in Production
 		if (WP_ENV == 'production') {
 			add_filter('acf/settings/show_admin', '__return_false');
 		}
-		
+
 		// Set local json directory
-		add_filter('acf/settings/save_json', function($path) {
+		add_filter('acf/settings/save_json', function($path) use(&$theme_dir) {
 			// update path
-			$path = get_stylesheet_directory() . '/assets/acf-json';
+			$path = $theme_dir;
 			// return
 			return $path;
 		});
+
+    add_action('init', function() use(&$theme_dir, &$plugin_dir) {
+      if(!file_exists($theme_dir)) {
+        mkdir($theme_dir);
+        copy($plugin_dir . '/.gitkeep', $theme_dir . '/.gitkeep');
+      }
+    });
 
 	}
 
 }
 
- ?>
