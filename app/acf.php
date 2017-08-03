1130599deeb89dae451f0dc92759ad381c2356dc
Michael W. Delaney <michael.delaney@gmail.com>
2017-08-03 09:21:48 -0400

    Update acf.php

--- app/acf.php
+++ app/acf.php
@@ -10,6 +10,14 @@ class ACF {
 		if (WP_ENV == 'production') {
 			add_filter('acf/settings/show_admin', '__return_false');
 		}
+		
+		// Set local json directory
+		add_filter('acf/settings/save_json', function($path) {
+			// update path
+			$path = get_stylesheet_directory() . '/acf-json';
+			// return
+			return $path;
+		});
 
 	}
 
