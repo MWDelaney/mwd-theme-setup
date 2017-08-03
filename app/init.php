3a1f1f49ea2b6d4d32cb25b303fb64bef0580307
Michael W. Delaney <michael.delaney@gmail.com>
2017-08-03 15:19:15 -0400

    Update init.php

--- app/init.php
+++ app/init.php
@@ -4,30 +4,14 @@ namespace MWD\Setup;
 // Set up plugin class
 class Init {
 
-	function __construct() {
-		/**
-	   * Flatfile includes
-	   *
-	   * The $includes array determines the code library included in your theme.
-	   * Add or remove files to the array as needed. Supports child theme overrides.
-	   *
-	   * Please note that missing files will produce a fatal error.
-	   *
-	   */
-		 $includes = [
-		   'app/wp_bootstrap_navwalker.php',   // Bootstrap 3 Navwalker
-		   'app/wp_bootstrap4_navwalker.php',   // Bootstrap 4 Navwalker
-		   'app/wp_bootstrap_pagination.php',  // Bootstrap 3 Pagination
-		   'app/wp_bootstrap4_pagination.php',  // Bootstrap 4 Pagination
-		 ];
+  function __construct() {
+  
+    /**
+    * Flatfile includes
+    */
+    foreach(glob(MWD_SETUP_PLUGIN_DIR . 'app/includes/*.php') as $inc) {
+      require_once($inc);
 
-		 foreach ($includes as $file) {
-		   if ( !$filepath = MWD_SETUP_PLUGIN_DIR . $file ) {
-		     trigger_error(sprintf(__('Error locating %s for inclusion', 'sage'), $file), E_USER_ERROR);
-		   }
-		   require_once $filepath;
-		 }
-		 unset($file, $filepath);
-	}
-
+    }
+  }
 }
